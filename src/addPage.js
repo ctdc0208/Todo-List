29f984c5f7b1c7a546985e3e9c856f5e110b85fc
nexus0208 <christiandelacruz0208@gmail.com>
2021-09-11 17:29:26 +0800

    add tab browsing on

--- src/addPage.js
+++ src/addPage.js
@@ -8,6 +8,19 @@ class pages {
   }
 }
 
+// add tab browsing
+const openTab = (evt, tabName) => {
+  var i, tabcontent, tablinks;
+
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
+};
+
 // set arrays in element to display pages
 const displayPages = (() => {
   const pageList = document.querySelector('#table-body');
